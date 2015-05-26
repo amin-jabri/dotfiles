ad51fe79b0eb648a6db79241c7b48a60bcf83495
Mohamed Amin JABRI <amin.jabri@gmail.com>
2015-05-26 17:26:32 +0900

    adding grep aliases with colors

--- bash/aliases_bash.sh
+++ bash/aliases_bash.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# ls aliases
 alias ls='ls --color=auto'
 alias l='ls -CF'
 alias ll='ls -lhF'
 alias lla='ls -alhF'
 alias la='ls -A'
 alias l.='ls -d .*'
+
+# grep aliases
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 alias vim="vim -u ${HOME}/.vimrc -N -U NONE -X"
 alias g='git'
