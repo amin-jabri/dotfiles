d3e5baab5d20d5d7323c87d2a45850bd0e12cd12
Mohamed Amin JABRI <amin.jabri@gmail.com>
2015-12-02 11:21:57 +0900

    set homebrew cask symlink dir to /Applications as default

--- bash/bashrc_after-osx.sh
+++ bash/bashrc_after-osx.sh
@@ -8,6 +8,11 @@ fi
 # homebrew bash completion
 source `brew --prefix`/Library/Contributions/brew_bash_completion.sh
 
+# brew-cask symlink into /Applications instead of ~/Applications
+# override on the command line when installing:
+# brew cask install --appdir="~/Applications" google-chrome
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+
 # set our PATH
 PATH="/usr/local/opt/coreutils/libexec/gnubin:/usr/local/sbin:/usr/local/bin:$PATH"
 PATH="$(brew --prefix php55)/bin:$PATH"
