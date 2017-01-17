cc957054908d9d622d6fb55cc8add8f1acc13ca0
Mohamed Amin JABRI <amin.jabri@gmail.com>
2017-01-17 11:58:50 +0900

    Symbolic-Link gen_cscope_tags.sh in ~/bin.

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -16,6 +16,11 @@ fi
 # -p says to create the dir if it doesn't exist already
 mkdir -p $HOME/bin
 
+if [[ ! -a ~/bin/gen_cscope_tags.sh ]]
+then
+  ln -f -s $script_directory/misc/gen_cscope_tags.sh ~/bin/gen_cscope_tags.sh
+fi
+
 source $script_directory/bash/setup_bash.sh
 source $script_directory/gitconfig_setup.sh
 source $script_directory/tmux/tmux_setup.sh
