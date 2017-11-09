2bba01ccc785aaf27801457b6eaa4e61e7834829
Mohamed Amin JABRI <amin.jabri@gmail.com>
2017-11-09 12:14:33 +0900

    Add a trap to kill ssh-agent on exit

--- bash/bashrc_after-linux.sh
+++ bash/bashrc_after-linux.sh
@@ -5,3 +5,6 @@ if [ -z "${SSH_AUTH_SOCK}" ] ; then
 	eval `ssh-agent -s` >/dev/null 2>&1
 	# ssh-add
 fi
+
+# kill ssh-agent when exiting
+trap 'test -n "${SSH_AUTH_SOCK}" && eval `/usr/bin/ssh-agent -k`' 0
